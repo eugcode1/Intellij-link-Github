a279ba0fa8843d0ba25e574285e7a07bc49205a9
euglin1130 <caoqijia1130@gmail.com>
2017-02-17 14:15:13 -0800

    save changes

--- src/link.java
+++ src/link.java
@@ -4,6 +4,6 @@
 public class link {
     public static void main(String[] args) {
         System.out.println("linked");
-        System.out.println("linked");
+        System.out.println("linked23");
     }
 }
